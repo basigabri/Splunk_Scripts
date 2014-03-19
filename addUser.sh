55aaf7352cdbe9f2350b4c717bf7e00a57e6f104
Basilis Chatzigavriil <e.support1@tid.es>
2014-03-19 14:15:41 +0100

    changes commited

--- addUser.sh
+++ addUser.sh
@@ -8,24 +8,24 @@ read -p "enter passwd: "  passwd
 #Use REST api to add users
 
 
-curl -k -u admin:splunk@telefonica123 https://10.26.204.145:8089/services/authorization/roles \
+curl -k -u admin:changeme https://10.26.204.145:8089/services/authorization/roles \
 -d name=$user \
 -d imported_roles=power \
 -d srchIndexesAllowed=$index
 
 
-curl -k -u admin:splunk@telefonica123 https://10.26.204.146:8089/services/authorization/roles \
+curl -k -u admin:changeme https://10.26.204.146:8089/services/authorization/roles \
 -d name=$user \
 -d imported_roles=power \
 -d srchIndexesAllowed=$index
- curl -k -u admin:splunk@telefonica123 https://10.26.204.145:8089/services/authentification/users \
+ curl -k -u admin:changeme https://10.26.204.145:8089/services/authentification/users \
 -d name=$user \
 -d realname=$real \
 -d password=$passwd \
 -d defaultApp=search \
 -d roles=$user
 
- curl -k -u admin:splunk@telefonica123 https://10.26.204.146:8089/services/authentification/users \
+ curl -k -u admin:changeme https://10.26.204.146:8089/services/authentification/users \
 -d name=$user \
 -d realname=$real \
 -d password=$passwd \
